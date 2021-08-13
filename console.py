f60bdf9413ea7cbe45e13fb51f915bc5cb5ae7ce
HelloMaggieP <m.pentcheva@hotmail.com>
2021-08-13 14:54:44 +0100

    psudo code in console

--- console.py
+++ console.py
@@ -1,3 +1,9 @@
 #import models
 #import repositories
-#import 
+#import db blueprint 
+
+#create instances of models - city here
+#create instances of models - city here
+#create instances of models - country here
+#create instances of models - country here
+
