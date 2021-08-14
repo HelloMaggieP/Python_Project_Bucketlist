1f18f616dfa6aeeef7ae1c8c8439fc3a8ef88ca5
HelloMaggieP <m.pentcheva@hotmail.com>
2021-08-14 12:59:04 +0100

    test files set up. first test working

--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -1,16 +1,27 @@
 from db.run_sql import run_sql
-
 from models.country import Country
 from models.city import City
-
 import repositories.city_repository as city_repo
 
+def select_all(country):
+    countries = []
 
-# all countries 
+    sql = "SELECT * FROM countries"
+    results = run_sql(sql)
+
+    for row in results:
+        country = Country(row['name'], row['id'])
+        countries.append(country)
+    return countries
+
 
 # new country
 #get country
 # post country
+def add_country():
+    pass
 
 
 #delete country
+def delete():
+    pass
