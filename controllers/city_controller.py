1cd295b79ecb67c61ff2e8eef671f9d117fa334b
HelloMaggieP <m.pentcheva@hotmail.com>
2021-08-17 13:02:09 +0100

    made route for adding

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -44,9 +44,20 @@ def create_entry():
 @cities_blueprint.route("/cities/<id>/edit", methods=['GET'])
 def edit_cities(id):
     city = city_repo.select(id)
-    return render_template('cities/edit/html', city=city)
+    return render_template('cities/edit.html', city=city)
 
 #UPDATE / PUT 'cities/<id>
+@cities_blueprint.route("cities/<id>", methods = ['POST'])
+def update_city(id):
+    country_id      = request.form['country_id']
+    city            = request.form['city']
+    film_locations  = request.form['film_locations']
+    visited         = request.form['visited']
+    country         = country_repo.select(country_id)
+    updated_city    = City(city, film_locations, country, visited)
+    city_repo.save(updated_city)
+    return redirect("/cities")
+
 
 @cities_blueprint.route("/cities/visited")
 def select_visited():
